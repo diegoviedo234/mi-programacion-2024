a9666fd6e56049fb901b2171b56bcd16f96c200b
Diego Ariel <diego.ariel234@gmail.com>
2024-07-21 19:33:41 -0600

    Practica 3

--- miSegundaPractica/miSegundaPractica/miSegundaPractica/Properties/AssemblyInfo.cs
+++ miSegundaPractica/miSegundaPractica/miSegundaPractica/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("miSegundaPractica")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("miSegundaPractica")]
 [assembly: AssemblyCopyright("Copyright © HP 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("04656388-b0b8-4a72-bfa2-a99686a445d9")]
+[assembly: Guid("84fda9bc-ae71-437f-9c0a-262b22c5b6ee")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
