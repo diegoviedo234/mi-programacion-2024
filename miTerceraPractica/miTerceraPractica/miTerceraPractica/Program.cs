0f225026742efc56b71c6d36988f3c9d05d843d1
Diego Ariel <diego.ariel234@gmail.com>
2024-07-21 19:28:23 -0600

    Desarrolle un programa que pida números positivos al usuario, y vaya calculando la suma de todos ellos (terminará cuando se teclea un número negativo o cero.

---
+++ miTerceraPractica/miTerceraPractica/miTerceraPractica/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miTerceraPractica
+{
+    internal class Program
+        
+    {
+        static void Main(string[] args)
+        // Desarrolle un programa que pida número positivos al usuario, y vaya calculando la suma de todo ello (terminará cuando se teclea un número negativo o cero).
+
+        {
+            int numero;
+            int suma = 0;
+
+            while (true)
+            {
+
+                Console.WriteLine("Introduzca un número positivo (Si desea salir, introduzca un número negativo o 0)");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero <= 0)
+                {
+                    break;
+                }
+
+                suma += numero;
+                Console.WriteLine($"Suma actual: {suma}");
+
+            }
+
+            Console.WriteLine($"La suma total de los números positivos que se introducieron es: {suma}");
+        }
+    }
+}
